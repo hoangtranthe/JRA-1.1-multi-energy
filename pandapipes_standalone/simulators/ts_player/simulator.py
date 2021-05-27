df963af620bd91a135f53d83c1c4cd80c629f3d3
WildC <s180026@student.dtu.dk>
2021-05-27 12:41:44 +0200

    Add copyright reference to files

--- pandapipes_standalone/simulators/ts_player/simulator.py
+++ pandapipes_standalone/simulators/ts_player/simulator.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2021 by ERIGrid 2.0. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 """
     Time series player/simulator
 """

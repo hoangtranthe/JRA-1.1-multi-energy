10bd44619f346b805e1f4d3ef75758852266fc55
WildC <s180026@student.dtu.dk>
2021-05-31 10:27:40 +0200

    Verified by stability test on pipe length and consumer heat load

--- pandapipes_standalone/mosaik_scenario_tc08_ts01.py
+++ pandapipes_standalone/mosaik_scenario_tc08_ts01.py
@@ -51,9 +51,9 @@ STORAGE_TANK_INIT_TEMP = 70  # Storage tank initial temperature
 STEP_SIZE = 60
 PHYSICAL_STEP_SIZE = STEP_SIZE  # Physical evolution [s]
 CONTROL_STEP_SIZE = STEP_SIZE  # Flow control [s]
-PIPEFLOW_STEP_SIZE = 60
+PIPEFLOW_STEP_SIZE = STEP_SIZE
 OPTIMIZER_STEP_SIZE = STEP_SIZE  # Used for MPC controller [s]
-END = 6 * 60 * 60
+END = 72 * 60 * 60
 
 # # Set which day we are looking at.
 consumer_demand_series = pd.read_csv('./resources/heat/tc08ts01/distorted_heat_demand_load_profiles.csv', index_col=0, parse_dates=True)

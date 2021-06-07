9fb898e0c1483d52553bd37f0eaaac8b5f305498
Tran The HOANG <41739778+hoangtranthe@users.noreply.github.com>
2021-06-07 10:58:20 +0200

    Update heatpump_control.py

--- pandapower_standalone/heatpump_control.py
+++ pandapower_standalone/heatpump_control.py
@@ -1,88 +1,77 @@
-__author__ = 'Tran The HOANG'
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from pandapower.control.basic_controller import Controller
+
+try:
+    import pplog as logging
+except ImportError:
+    import logging
+logger = logging.getLogger(__name__)
 
 
-from heatpump_control import HPController
-import math
-
-class DiscreteHPControl(HPController):
+class HPController(Controller):
     """
-    Heat Pump Controller with Heat Pump consumption-based voltage control.
+    Heat Pump Controller with Heat Pump consumption-based voltage control
+
     INPUT:
-        **net** (attrdict) - Pandapower struct
-        **hid** (int) - ID of the heat pump that is controlled
+       **net** (attrdict) - Pandapower struct
+
+       **hid** (int) - ID of the Heat Pupm that is controlled
+
+       **tol** (float) - Voltage tolerance band at bus in Percent
+
+       **in_service** (bool) - Indicates if the element is currently active
+
+
     OPTIONAL:
-        **tol** (float, 0.001) - Voltage tolerance band at bus in Percent (default: 1% = 0.01pu)
-        **in_service** (bool, True) - Indicates if the controller is currently in_service
-        **drop_same_existing_ctrl** (bool, False) - Indicates if already existing controllers of the same type and with the same matching parameters (e.g. at same element) should be dropped
+        **recycle** (bool, True) - Re-use of internal-data in a time series loop.
     """
 
-    def __init__(self, net, hid, delta_vm_upper_pu, delta_vm_lower_pu, deadband, load="load", tol=1e-3, in_service=True, order=0, drop_same_existing_ctrl=False,
-                 matching_params=None, **kwargs):
-        if matching_params is None:
-            matching_params = {"hid": hid, 'load': load}
-        super(DiscreteHPControl, self).__init__(
-            net, hid, deadband, tol=tol, in_service=in_service, order=order, load=load,
-            drop_same_existing_ctrl=drop_same_existing_ctrl, matching_params=matching_params,
-            **kwargs)
-
-        self.delta_vm_upper_pu = delta_vm_upper_pu
-        self.delta_vm_lower_pu = delta_vm_lower_pu
+    def __init__(self, net, hid, deadband, tol, in_service, level=0, order=0, recycle=True, load ="load", **kwargs):
+        super().__init__(net, in_service=in_service, level=level, order=order, recycle=recycle,
+                         **kwargs)
+        self.hid = hid
+        self.load = load
         self.deadband = deadband
-        self.hp_p_mw = net[self.load].at[hid, "p_mw"]
+
+        self.element_in_service = net[self.load].at[self.hid, "in_service"]
+
+        self.controlled_bus = net[self.load].at[hid, "bus"]
+        if self.controlled_bus in net.ext_grid.loc[net.ext_grid.in_service, 'bus'].values:
+            logger.warning("Controlled Bus is Slack Bus - deactivating controller")
+            self.set_active(net, False)
+        elif self.controlled_bus in net.ext_grid.loc[
+            ~net.ext_grid.in_service, 'bus'].values:
+            logger.warning("Controlled Bus is Slack Bus with slack out of service - "
+                           "not deactivating controller")
+
+
         self.hp_max_p_mw = net[self.load].at[hid, "max_p_mw"]
-        self.hp_min_p_mw = net[self.load].at[hid, "min_p_mw"]
+        self.hp_p_mw = net[self.load].at[hid, "p_mw"]
 
-    def control_step(self, net):
-        """
-        Implements one step of the Discrete controller, always stepping only one tap position up or down
-        """
+        self.tol = tol
+
+        self.set_recycle(net)
+
+    def set_recycle(self, net):
+        allowed_elements = ["load"]
+        if net.controller.at[self.index, 'recycle'] is False or self.load not in allowed_elements:
+            # if recycle is set to False by the user when creating the controller it is deactivated or when
+            # const control controls an element which is not able to be recycled
+            net.controller.at[self.index, 'recycle'] = False
+            return
+        # these variables determine what is re-calculated during a time series run
+        recycle = dict(load=True, gen=False, bus_pq=False)
+        net.controller.at[self.index, 'recycle'] = recycle
+
+    def timestep(self, net):
         self.hp_p_mw = net[self.load].at[self.hid, "p_mw"]
-        self.hp_max_p_mw = net[self.load].at[self.hid, "max_p_mw"]
-        self.hp_min_p_mw = net[self.load].at[self.hid, "min_p_mw"]
-        self.vmeas_pu = net.res_bus.at[self.controlled_bus, "vm_pu"]
-        delta_v_meas_pu = self.vmeas_pu - 1
 
-        if delta_v_meas_pu - self.delta_vm_upper_pu > self.deadband:
-            hp_p_setpoint_mw = self.hp_p_mw + 0.05 * self.hp_max_p_mw
+    def __repr__(self):
+        s = '%s of %s %d' % (self.__class__.__name__, self.load, self.hid)
+        return s
 
-            if hp_p_setpoint_mw < self.hp_max_p_mw:
-                self.hp_p_mw = hp_p_setpoint_mw
-
-            else:
-                self.hp_p_mw = self.hp_max_p_mw
-
-
-        elif delta_v_meas_pu - self.delta_vm_lower_pu < -self.deadband and self.hp_p_mw > self.hp_min_p_mw:
-            hp_p_setpoint_mw = self.hp_p_mw - 0.02 * self.hp_max_p_mw
-
-            if hp_p_setpoint_mw >= self.hp_min_p_mw:
-                self.hp_p_mw = hp_p_setpoint_mw
-
-            elif hp_p_setpoint_mw < self.hp_min_p_mw:
-                self.hp_p_mw = 0
-
-
-        elif delta_v_meas_pu - self.delta_vm_lower_pu < -self.deadband and self.hp_p_mw <= self.hp_min_p_mw:
-            self.hp_p_mw = 0
-
-        net[self.load].at[self.hid, "p_mw"] = self.hp_p_mw
-
-    def is_converged(self, net):
-        """
-        Checks if the voltage is within the desired voltage band, then returns True
-        """
-        if not self.hid in net[self.load].index or \
-           not net[self.load].at[self.hid, 'in_service']:
-            return True
-
-        self.vmeas_pu = net.res_bus.at[self.controlled_bus, "vm_pu"]
-        delta_v_meas_pu = self.vmeas_pu - 1
-
-
-        if delta_v_meas_pu > self.delta_vm_upper_pu + self.deadband and net[self.load].at[self.hid, "p_mw"] == self.hp_max_p_mw:
-            return True
-
-        elif delta_v_meas_pu < self.delta_vm_lower_pu - self.deadband and net[self.load].at[self.hid, "p_mw"] == 0:
-            return True
-
-        return self.delta_vm_lower_pu - self.deadband <= delta_v_meas_pu <= self.delta_vm_upper_pu + self.deadband
+    def __str__(self):
+        s = '%s of %s %d' % (self.__class__.__name__, self.load, self.hid)
+        return s
